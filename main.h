c333d8352f07b5d57e960b3dd4ecb26a7d868c0a
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-09 22:27:20 +0200

    Added exe function

--- main.h
+++ main.h
@@ -1,6 +1,19 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+extern char **environ;
+
+/**
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
+ */
+typedef struct list_s
+{
+  char *dir;
+  struct list_s *next;
+} list_t;
+
 int tokenize(char *s);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
