eabad7e070e5c539318949ddc780edcddfd6d4d9
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-09 22:53:40 +0200

    Added a function to frr=ee args

---
+++ args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * free_args - Frees up memory.
+ * @args: char
+ * @first: char
+ */
+
+void free_args(char **args, char **first)
+{
+	size_t i;
+  
+  i = 0;
+	while (args[i] || args[i + 1])
+  {
+		free(args[i]);
+    i++;
+  }
+
+	free(first);
+}
