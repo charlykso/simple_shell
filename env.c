7bd965c9ed3aca9ddb4ea001db744864672ef2e1
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-10 20:53:22 +0200

    Added get env

--- env.c
+++ env.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "main.h"
 
+void env_free(void);
+char **_getenv(const char *var);
+
 /**
  * env_free - Frees the the environ.
  */
@@ -18,4 +21,26 @@ void env_free(void)
     i++;
   }
 	free(environ);
+}
+
+/**
+ * _getenv - Gets an environmental variable from the PATH.
+ * @var: char
+ * Return: environ does not exist - NULL.
+ *         else environ.
+ */
+char **_getenv(const char *var)
+{
+	int i, len;
+
+	len = _strlen(var);
+	
+	i = 0;
+	while (environ[i])
+	{
+		if (_strncmp(var, environ[i], len) == 0)
+			return (&environ[i]);
+	}
+
+	return (NULL);
 }
